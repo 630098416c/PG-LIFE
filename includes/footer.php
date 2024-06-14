5443f086fa4b28c6435e45ea7f77b84d3f96c5da
charan sai <saic78999@gmail.com>
2024-06-14 14:07:05 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,22 @@
+<div class="footer">
+        <div class="page-container footer-container">
+            <div class="footer-cities">
+                <div class="footer-city">
+                    <a href="property_list.html">PG in Delhi</a>
+                </div>
+                <div class="footer-city">
+                    <a href="property_list.html">PG in Mumbai</a>
+                </div>
+                <div class="footer-city">
+                    <a href="property_list.html">PG in Bangalore</a>
+                </div>
+                <div class="footer-city">
+                    <a href="property_list.html">PG in Hyderabad</a>
+                </div>
+            </div>
+            <div class="footer-copyright"> 2020 Copyright PG Life </div>
+        </div>
+    </div>
+
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
